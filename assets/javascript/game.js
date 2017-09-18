2cbe5dbcc93e8f9bffeb4c77a8f3099dfb961e3c
Sara Hjelt <sarahjelt@Saras-MacBook-Air.local>
2017-09-18 18:19:10 -0400

    random number function works. that's about it.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,10 +12,38 @@
 //if totalScore > randomNumber, loss++ and generate new randomNumber and new gem values
 //if totalScore = randomNumber, win++ and generate new randomNumber and new gem values
 
+//id
+$(document).ready(function(){
+
+//range from 19-120
+var randomNumber = Math.floor(Math.random() * 102) + 19;
+var gemRandom = Math.floor(Math.random() * 12) + 1;
+
+var totalScore = 
+
+render();
+
+
+function assignValue() {
+  
+}
 
 function render() {
-document.getElementById("random-number").innerHTML = randomNumber;
-document.getElementById("total-score").innerHTML = totalScore;
-document.getElementById("win").innerHTML = win;
-document.getElementById("loss").innerHTML = loss;
+  document.getElementById("random-number").innerHTML = randomNumber;
+  console.log(randomNumber);
+
+  document.getElementById("total-score").innerHTML = totalScore;
+  console.log(totalScore);
+
+// document.getElementById("win").innerHTML = win;
+// document.getElementById("loss").innerHTML = loss;
 }
+
+$(".gem-img").on("click", function() {
+        var gem = $(".gem-img");
+        gem.text($(this).attr(gemRandom));
+        $("#gems").append(gem);
+      });
+
+
+});
